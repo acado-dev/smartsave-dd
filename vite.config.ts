1cc1364bae72fe2cfeac9f5c9f0d3c630a1578eb
mohit-ev <mohit.kumar@Edulystventures.com>
2026-02-21 01:22:16 +0530

    build

--- vite.config.ts
+++ vite.config.ts
@@ -50,7 +50,7 @@ export default defineConfig(({ mode }) => ({
             },
             devOptions: {
                 enabled: true,
-                type: 'module',
+                // type: 'module',
             },
         }),
     ].filter(Boolean),
